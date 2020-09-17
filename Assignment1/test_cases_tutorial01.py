4c641dc22eac951fe8cc3e2adb875f652cfc638f
khushigour <khushigour0301@gmail.com>
2020-09-17 14:57:27 +0530

    Done with the power function of calulator

--- Assignment1/test_cases_tutorial01.py
+++ Assignment1/test_cases_tutorial01.py
@@ -1,19 +1,28 @@
 import tutorial01 as A1
 
-actual_answers = [9, 12, 80, 5]
+actual_answers = [27, 0, -0.037]
 student_answers = []
 
-test_case_1 = A1.add(4, 5)
+# test_case_1 = A1.add(4, 5)
+# student_answers.append(test_case_1)
+
+# test_case_2 = A1.subtract(14, 2)
+# student_answers.append(test_case_2)
+
+# test_case_3 = A1.multiply(10,8)
+# student_answers.append(test_case_3)
+
+# test_case_4 = A1.divide(10,2)
+# student_answers.append(test_case_4)
+
+test_case_1 = A1.power(3,3)
 student_answers.append(test_case_1)
 
-test_case_2 = A1.subtract(14, 2)
+test_case_2 = A1.power(3,"aa")
 student_answers.append(test_case_2)
 
-test_case_3 = A1.multiply(10,8)
+test_case_3 = A1.power(-3,-3)
 student_answers.append(test_case_3)
-
-test_case_4 = A1.divide(10,2)
-student_answers.append(test_case_4)
 
 print(actual_answers)
 print(student_answers)
--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -18,3 +18,41 @@ def divide(num1, num2):
 	division = num1/num2
 	return division
 	
+# Function to add power function
+#You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2):
+	if(type(num2)==float or type(num2)==int) and (type(num1)==int or type(num2)==float):
+		power = num1
+		num2 = int(num2)
+		if num2==0:
+			return 1
+		if num2>0:
+			while num2>1:
+				power*= num1
+				num2 -=1
+		if num2<0:
+			while num2<=0:
+				power/= num1
+				num2 += 1
+		return round(power,3)
+	else:
+		return 0
+	
+# Python 3 program to print GP.  geometric Progression
+#You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n): 
+	gp=[]
+	return gp 
+
+# Python 3 program to print AP.  arithmetic Progression
+#You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n): 
+	ap=[]
+	return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n): 
+	hp=[]
+	return hp
+
