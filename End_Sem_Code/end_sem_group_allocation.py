342f8a62499d0ed320b2eec47a6aad92e8503c36
khushigour <khushigour0301@gmail.com>
2020-11-29 19:44:28 +0530

    Part-1 Branch.csv file is created in Endsem asgn

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -27,6 +27,15 @@ def group_allocation(filename, number_of_groups):
     df_Strength= df_Strength.sort_values(by =['STRENGTH', 'BRANCH_CODE'] , ascending=[0,1])
     path_1 = os.path.join(initial_path,r'Batch_Strength.csv')
     df_Strength.to_csv(path_1, header = True, index = False)
+
+    # Part-2 Branch.csv file is created
+    for i in Branches:
+        df_Branch = df[df['Branch']==i]
+        df_Branch = df_Branch.iloc[:,:-1]
+        df_Branch = df_Branch.sort_values(by = 'Roll')
+        Branch_name = i +'.csv'
+        path_2 = os.path.join(initial_path,Branch_name)
+        df_Branch.to_csv(path_2, header = True, index = False)
     pass
 
 filename = "Btech_2020_master_data.csv"
