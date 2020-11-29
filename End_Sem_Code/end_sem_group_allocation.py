51a29b18eceef17ba5c5ee89f0586b353f54e120
khushigour <khushigour0301@gmail.com>
2020-11-29 19:42:55 +0530

    Part-1 Batch_Strength file is created in Endsem asgn

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -17,7 +17,16 @@ def del_create_analytics_folder():
 def group_allocation(filename, number_of_groups):
     # Entire Logic 
     del_create_analytics_folder()
-    
+    initial_path = os.path.join(os.getcwd(),r'groups')
+    df = pd.read_csv(filename)
+    df['Branch'] = df['Roll'].str.extract(r'([A-Za-z]{2})')
+    Branches = df['Branch'].unique()
+
+    # Part-1 Batch_Strength file is created
+    df_Strength = pd.DataFrame(df['Branch'].value_counts().reset_index().values,columns =['BRANCH_CODE', 'STRENGTH'])
+    df_Strength= df_Strength.sort_values(by =['STRENGTH', 'BRANCH_CODE'] , ascending=[0,1])
+    path_1 = os.path.join(initial_path,r'Batch_Strength.csv')
+    df_Strength.to_csv(path_1, header = True, index = False)
     pass
 
 filename = "Btech_2020_master_data.csv"
