c3977243570ae54fd747c967fa52a1511fc3fb6f
khushigour <khushigour0301@gmail.com>
2020-12-03 14:28:17 +0530

    Making a window in notepad project

---
+++ CS384_Final_Structure/Projects/P2_Notepad_GUI/p2_main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+from tkinter import messagebox
+import os
+import time
+
+root = Tk()
+root.title('Notepad Project')
+
+
+root.mainloop()
